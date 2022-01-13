17993da8f42b348efe50a91df28295efd27ce52e
Sehan Shetty <59560852+yttehs123@users.noreply.github.com>
2022-01-13 16:08:00 +0530

    update dqn

--- Digital_car_and_map_and_AI/DQN_AI.py
+++ Digital_car_and_map_and_AI/DQN_AI.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
+#Import libraries
 
+import numpy as np
+import random
+import os 
+import torch
+import torch.nn
+import torch.nn.functional as fn
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+#architecture of neural network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size=input_size
+        self.nb_action=nb_action
+        self.fc1=nn.Linear(input_size, 30)#size of first layer, size of second layer(hidden)
+        self.fc2=nn.Linear(30, nb_action)
+        
+    def forward(self, state):
+        x = fn.relu(self.fc1(state))
+        q_values=self.fc2(x)
+        return q_values
+    
+#experience replay 
+        
